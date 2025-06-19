c3505d0d7b44dfaf3c7c832e84e5157e75bce12b
TN23VN <tronghv9abg@gmail.com>
2025-06-20 03:49:29 +0700

    commit -m"Update UI"

---
+++ Assets/script/NPC/NPCShop.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class NPCShop : MonoBehaviour
+{
+    public GameObject shopUI;
+
+    private bool playerInRange = false;
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            shopUI.SetActive(!shopUI.activeSelf);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            //Show "Nhấn E để giao dịch"
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            shopUI.SetActive(false);
+        }
+    }
+}
